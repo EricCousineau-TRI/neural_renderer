bdd25c82f7bde935c904cc944cae89733da9044a
Eric Cousineau <eric.cousineau@tri.global>
2021-07-12 15:44:49 -0400

    freezing... add tracers

--- examples/example1.py
+++ examples/example1.py
@@ -14,7 +14,40 @@ import neural_renderer as nr
 current_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(current_dir, 'data')
 
+def reexecute_if_unbuffered():
+    """Ensures that output is immediately flushed (e.g. for segfaults).
+    ONLY use this at your entrypoint. Otherwise, you may have code be
+    re-executed that will clutter your console."""
+    import os
+    import shlex
+    import sys
+    if os.environ.get("PYTHONUNBUFFERED") in (None, ""):
+        os.environ["PYTHONUNBUFFERED"] = "1"
+        argv = list(sys.argv)
+        if argv[0] != sys.executable:
+            argv.insert(0, sys.executable)
+        cmd = " ".join([shlex.quote(arg) for arg in argv])
+        sys.stdout.flush()
+        os.execv(argv[0], argv)
 
+
+def traced(func, ignoredirs=None):
+    """Decorates func such that its execution is traced, but filters out any
+     Python code outside of the system prefix."""
+    import functools
+    import sys
+    import trace
+    if ignoredirs is None:
+        ignoredirs = ["/usr", sys.prefix]
+    tracer = trace.Trace(trace=1, count=0, ignoredirs=ignoredirs)
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        return tracer.runfunc(func, *args, **kwargs)
+
+    return wrapped
+
+@traced
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--filename_input', type=str, default=os.path.join(data_dir, 'teapot.obj'))
@@ -52,4 +85,5 @@ def main():
     writer.close()
 
 if __name__ == '__main__':
+    reexecute_if_unbuffered()
     main()
